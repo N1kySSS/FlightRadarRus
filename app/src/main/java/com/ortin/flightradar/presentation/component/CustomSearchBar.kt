517c54e8189e44ffe77c82f1fae8d9a65bd0736e
Nikita Savvin <nik.savvin.2004@mail.ru>
2025-03-13 22:36:46 +0300

    Add custom TopAppBar with custom SearchBar

---
+++ app/src/main/java/com/ortin/flightradar/presentation/component/CustomSearchBar.kt
@@ -0,0 +1,84 @@
+package com.ortin.flightradar.presentation.component
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import com.ortin.flightradar.R
+import com.ortin.flightradar.ui.theme.Primary
+
+@Composable
+fun CustomSearchBar(
+    onValueChanged: (String) -> Unit,
+    value: String,
+    modifier: Modifier = Modifier,
+    placeholderColor: Color = Color.Gray,
+    focusedIconsColor: Color = Primary,
+    unfocusedIconsColor: Color = Color.Black,
+    focusedIndicatorColor: Color = Primary,
+    unfocusedIndicatorColor: Color = Color.White,
+    focusedContainerColor: Color = Color.White,
+    unfocusedContainerColor: Color = Color.White
+) {
+    val focusManager = LocalFocusManager.current
+
+    OutlinedTextField(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 4.dp),
+        value = value,
+        onValueChange = onValueChanged,
+        placeholder = { Text(text = "FlightRadarRus") },
+        singleLine = true,
+        shape = RoundedCornerShape(10.dp),
+        leadingIcon = {
+            Icon(
+                painter = painterResource(R.drawable.search),
+                contentDescription = "Поиск",
+            )
+        },
+        trailingIcon = {
+            IconButton(
+                onClick = { onValueChanged("") },
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.delete),
+                    contentDescription = "Стереть текст",
+                )
+            }
+        },
+        colors = TextFieldDefaults.colors(
+            unfocusedIndicatorColor = unfocusedIndicatorColor,
+            focusedIndicatorColor = focusedIndicatorColor,
+            unfocusedLeadingIconColor = unfocusedIconsColor,
+            unfocusedTrailingIconColor = unfocusedIconsColor,
+            focusedLeadingIconColor = focusedIconsColor,
+            focusedTrailingIconColor = focusedIconsColor,
+            focusedContainerColor = focusedContainerColor,
+            unfocusedContainerColor = unfocusedContainerColor,
+            focusedPlaceholderColor = placeholderColor,
+            unfocusedPlaceholderColor = placeholderColor,
+        ),
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Done
+        ),
+        keyboardActions = KeyboardActions(
+            onDone = {
+                focusManager.clearFocus()
+            }
+        ),
+    )
+}
---
+++ app/src/main/java/com/ortin/flightradar/presentation/component/CustomTopAppBar.kt
@@ -0,0 +1,82 @@
+package com.ortin.flightradar.presentation.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ortin.flightradar.R
+
+@Composable
+fun CustomTopAppBar(
+    onValueChanged: (String) -> Unit,
+    value: String,
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+
+    Row(
+        modifier = Modifier
+            .height(80.dp)
+            .fillMaxWidth()
+            .background(color = Color.Black.copy(alpha = 0.4f))
+            .padding(horizontal = 16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        ClickableText(
+            text = AnnotatedString("AR"),
+            style = TextStyle(
+                color = Color.White,
+                fontSize = 26.sp,
+                fontWeight = FontWeight.W300,
+                fontStyle = FontStyle.Normal,
+                textAlign = TextAlign.Center,
+            ),
+            onClick = {
+                TODO("Add action")
+            },
+        )
+        CustomSearchBar(
+            onValueChanged = onValueChanged,
+            value = value,
+            modifier = Modifier.weight(1f)
+        )
+        Icon(
+            modifier = Modifier
+                .clickable(
+                    interactionSource = interactionSource,
+                    indication = null
+                ) {
+                    TODO("Add action")
+                },
+            painter = painterResource(R.drawable.profile),
+            contentDescription = "Profile",
+            tint = Color.White
+        )
+    }
+}
+
+@Preview
+@Composable
+fun TopAppBarPreview() {
+    CustomTopAppBar({}, "")
+}
