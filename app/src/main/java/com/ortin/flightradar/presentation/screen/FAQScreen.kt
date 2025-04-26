1f7c9e2a6ad1b74791ce19196b09b0977e818a1c
Nikita Savvin <nik.savvin.2004@mail.ru>
2025-04-26 23:56:46 +0300

    Add stub on the FAQ screen

--- app/src/main/java/com/ortin/flightradar/presentation/screen/FAQScreen.kt
+++ app/src/main/java/com/ortin/flightradar/presentation/screen/FAQScreen.kt
@@ -1,11 +1,15 @@
 package com.ortin.flightradar.presentation.screen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 import com.ortin.flightradar.presentation.component.header.CustomHeader
 
 @Composable
@@ -16,10 +20,18 @@ fun FAQScreen(
         modifier = Modifier
             .fillMaxSize()
             .background(color = Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         CustomHeader(
             name = "FAQ",
             onBackPressed = onBackPressed
         )
+        Box(modifier = Modifier.fillMaxSize()) {
+            Text(
+                modifier = Modifier.align(Alignment.Center),
+                text = "Coming soon...",
+                fontSize = 24.sp
+            )
+        }
     }
 }
